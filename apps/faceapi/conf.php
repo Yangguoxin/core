0dc3a67a069f6ed85aaff61182ecff1cb6d05878
Yangguoxin <825656776@qq.com>
2017-07-06 08:58:27 +0800

    fix bug

--- apps/faceapi/conf.php
+++ apps/faceapi/conf.php
@@ -1,7 +1,7 @@
 <?php
     $face_server_conf = array(
-        "SERVER" => "http://112.74.25.82/api/",
-        "API_KEY"=> "testkey001",
-        "API_SECRET"=> "209e8934-dd70-4c00-aad0-019b21396eca"
+        "SERVER" => "",
+        "API_KEY"=> "",
+        "API_SECRET"=> ""
     );
 ?>
